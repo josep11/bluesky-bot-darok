5a387bffaf19fbd26ce19ac9d2eaea92f763fedc
Josep Alsina <josepalsinasio@gmail.com>
2025-05-15 08:54:30 +0200

    debug config

--- src/config/config.js
+++ src/config/config.js
@@ -9,7 +9,7 @@ export const REPLIE_INTERVAL_IN_SECONDS = 15 //QUANTIDADE EM SEGUNDOS PARA EFETU
 
 /*****************************************************      LIKES     *********************************************************************************/
 export const ENABLE_LIKES = true; // ATIVAR/DESATIVAR LIKES
-export const LIKE_INTERVAL_IN_SECONDS = 15 //QUANTIDADE EM SEGUNDOS PARA EFETUAR CADA LIKE -- FORTEMENTE SUGERIDO A DEIXAR MAIOR OU IGUAL A 15
+export const LIKE_INTERVAL_IN_SECONDS = 21 //QUANTIDADE EM SEGUNDOS PARA EFETUAR CADA LIKE -- FORTEMENTE SUGERIDO A DEIXAR MAIOR OU IGUAL A 15
 /*****************************************************      LIKES     *********************************************************************************/
 
-export const DETAILED_LOGS = false; // ATIVAR/DESATIVAR LOGS DETALADOS
+export const DETAILED_LOGS = true; // ATIVAR/DESATIVAR LOGS DETALADOS
